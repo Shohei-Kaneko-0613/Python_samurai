448b6fe77cc641bf5e99b038b7371604b715c997
shohei <c373ism@yahoo.co.jp>
2018-07-29 11:32:29 +0900

    create D1.py

---
+++ D1.py
@@ -0,0 +1,13 @@
+class Shape:
+    def __init__(self,s):
+        self.s=s
+    def draw(self):
+        
+        for i in range(1,self.s+1):
+            for j in range(1,self.s+1):
+                if i+j <= self.s+1:
+                    print("#",end="")
+            print()
+
+Shape1 = Shape(5)
+Shape1.draw()

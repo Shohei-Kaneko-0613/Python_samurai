184a8817aa2d48c2ef16e986c820d51bda4dcab5
shohei <c373ism@yahoo.co.jp>
2018-08-06 22:54:40 +0900

    modified A32.py

--- A32.py
+++ A32.py
@@ -1,8 +1,8 @@
 a=[5,3,1,2,4,6]
-print(sorted(a))
-print(sorted(a,reverse=True))
+print(sorted(a),"昇順")
+print(sorted(a,reverse=True),"降順")
 print("--------")
 a.sort()
-print(a)
+print(a,"昇順")
 a.reverse()
-print(a)
+print(a,"降順")

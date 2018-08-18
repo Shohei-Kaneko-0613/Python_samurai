3eb8815acaf84b55ea6a33f0b724a54415db1f8c
キャミさま <kyamisama@kyamisamanoMacBook-puro.local>
2018-08-19 07:50:30 +0900

    modify A45.py

--- A45.py
+++ A45.py
@@ -2,13 +2,11 @@ import re
 def number_count(x):
     s=0
     z=0
-    for m in re.finditer("\d{1,}",x): #整数のみ探す
+    for m in re.finditer('\d+\.*\d*',x): #整数のみ探す
         print(m.group())
-        s+=int(m.group())
-    for n in re.finditer("\d+(\.\d+)",x): #小数のみ探す
-        print(n.group())
-        z+=float(n.group())
-    return s+z
+        s+=float(m.group())
+
+    return s
     
 x="I am 20 years old and 2nd elder brother. I like football and am a fun of 99ers."
 y="11.1 3.5 Cumulus Networks has helped over 1000 companies build modern, web-scale networks using our innovative, open networking technology. In fact, 34% of the Fortune 50 have already adopted Cumulus Linux."

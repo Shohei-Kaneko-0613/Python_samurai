d44570755a272a58b89369a1656fdafd69e9b9be
キャミさま <kyamisama@kyamisamanoMacBook-puro.local>
2018-08-22 22:42:00 +0900

    modify D9.py

--- D9.py
+++ D9.py
@@ -23,23 +23,22 @@ class FileParser:
             
     def analyze(self):
         with open(self.filepath) as f:
-            x=f.read() #D8.txtの読み込み
-        m=re.findall(self.keyword,x) #D8.txtからkeywordマッチをmに代入
-        self.count=len(m) #マッチした数をカウント
-        return m[0] #配列となっているkeywprdを１つに纏める
-
+            x=f.read() 
+        m=re.findall(self.keyword,x)
+        self.count=len(m)
+        print(self.count)
+        
     def saveNoKeyword(self):
         with open(filepath) as f:
-            x=f.read() #D8.txtの読み込み
-        with open(filepath, mode='w',newline="\n") as f:
-            f.write(x) #D9-1にD8.txtをコピー
-        y=x.replace(self.analyze(),"") 
-        with open(self.filepathTo, mode='w',newline="\n") as f:
-            f.write(y) #D9-1からkeywordを削除
+            x=f.read().replace(self.keyword,"")
+        with open(filepathTo, mode='w',newline="\n") as f:
+            f.write(x)
+            return True
 
 filepath="D8.txt"
 keyword="https"
 count=0
 filepathTo="D9-1"
 fileparser=FileParser(filepath,keyword,count,filepathTo)
-fileparser.saveNoKeyword()
+fileparser.analyze()
+print(fileparser.saveNoKeyword())

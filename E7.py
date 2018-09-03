8e3ce2e826933e4ec819cbce76caa8364b1bd296
shohei <c373ism@yahoo.co.jp>
2018-09-03 20:51:20 +0900

    modify E7

--- E7.py
+++ E7.py
@@ -9,8 +9,11 @@ class MainHandler(tornado.web.RequestHandler):
 class MainHandler_post(tornado.web.RequestHandler):
     def post(self):
         text=self.get_argument('text1')
+        file="static/E7_POST_TEXT"
+        with open(file,"a")  as f:
+            f.write(text)
         self.render("static/E7.html")
-
+        
 
 def make_app():
     settings = {

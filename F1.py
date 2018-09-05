303c945e437ce796c4abf5bdab7419fa744d8bfa
shohei <c373ism@yahoo.co.jp>
2018-09-05 22:48:50 +0900

    modify F1

--- F1.py
+++ F1.py
@@ -6,19 +6,20 @@ import json
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("static/F1.html")
-'''        
+        
 class MainHandler_post(tornado.web.RequestHandler):
     def post(self):
-        num1=self.get_argument("check1")
-        num2=self.get_argument("check2")
-        num3=self.get_argument("check3")
-        num4=self.get_argument("check4")
-        num5=self.get_argument("check5")
+        num1=self.get_argument("check1","")
+        num2=self.get_argument("check2","")
+        num3=self.get_argument("check3","")
+        num4=self.get_argument("check4","")
+        num5=self.get_argument("check5","")
+        num_list=[]
         for num in num1,num2,num3,num4,num5:
-            x=num in str(on)
-            print(x)
-            self.render("static/F1.html",num=x)
-'''
+            if num == str(1):
+                num_list.append(num)
+        alert=len(num_list)
+        self.render("static/F1_alert.html",num=alert)
 
 def make_app():
     settings = {
@@ -26,7 +27,7 @@ def make_app():
 }
     return tornado.web.Application([
     (r"/", MainHandler),
-#    (r'/post_test/', MainHandler_post),
+    (r'/post_test/', MainHandler_post),
 ], **settings)
 
 if __name__ == "__main__":

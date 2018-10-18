8f18f9d538b645737b94dc276d4681a73c7bcefd
shohei <c373ism@yahoo.co.jp>
2018-10-18 23:23:46 +0900

    tornado HTTP POST

--- HTTP_POST.py
+++ HTTP_POST.py
@@ -2,21 +2,29 @@ import requests
 import urllib.parse
 import yaml
 import os
+import subprocess
 
-post_data = "test_post.py"
+r1 = requests.post("http://localhost:8888/post_test/", data={"files_data2":open("command.txt","rb").read()})
+response = (r1.content).decode()
+print(response)
 
-with open("parameter.yaml") as file:
-    yaml_file = yaml.load(file)
-    path = yaml_file["file_path"]
+if 1 <= len(response):
+    print("ファイルあります")
+    r3 = requests.post("http://localhost:8888/post_test2/", data={"files_data3":response})
+elif response == "NG":
+    print("ファイルありません")
+    post_data = "test_post.py"
 
-data = path + post_data
-print(data)
+    with open("parameter.yaml") as file:
+        yaml_file = yaml.load(file)
+        path = yaml_file["file_path"]
+
+    data = path + post_data
+    print(data)
 
 
-if os.path.exists(data):
-#    files = {"file": open(data,"rb")}
-    r = requests.post("http://localhost:8888/post_test/", files={"files_data":open("static/test_post.py","r")})
-    print(r.text,type(r))
-else:
-    print("file is does not exist")
-
+    if os.path.exists(data):
+        r2 = requests.post("http://localhost:8888/post_test3/", files={"files_data":open("static/test_post.py","r")})
+        print(r2.text,type(r2))
+    else:
+        print("file is does not exist")
--- tornado_post.py
+++ tornado_post.py
@@ -8,27 +8,42 @@ import subprocess
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("static/tornado_post_html.html")
-        
+
+#ファイルの有無を確認し、ファイルがあればファイルパスをレスポンスする        
 class MainHandler_post(tornado.web.RequestHandler):
     def post(self):
-#        post_data = self.get_argument("filename","test_post.py")
-        post_data = self.request.files["files_data"][0]["body"].decode()
-        print(type(post_data),post_data)
-        file="test.txt"
-#        with open(file) as data1:
-#            post_data2 = data1.read()
-#        print(post_data2) 
+        first_post = self.get_argument("files_data2")
+        print(first_post)
+        result = subprocess.check_output(["find", first_post, "-name","test_post2.py"])
+        print(result)
+        print(len(result))
+        if 1 <= len(result): 
+            self.write(result)
+        else:
+            self.write("NG")
 
-#        data = subprocess.check_output(["cat", post_data])
-        #print(data)
+#MainHandler_postでファイル有無の判定で有りの場合、ファイルの実行を行う
+class MainHandler_post2(tornado.web.RequestHandler):
+    def post(self):
+        second_post = self.get_argument("files_data3")
+        print(second_post)
+        subprocess.call(["python",second_post])
+
+#MainHandler_postの判定でファイルが無ければ、ファイルを作成して実行する
+class MainHandler_post3(tornado.web.RequestHandler):
+    def post(self):
+        post_data = self.request.files["files_data"][0]["body"].decode()
+
 
         DOWNLOAD_SAVE_DIR = "/Users/kanekoshohei/Documents/Git/Python/samurai/"
-        saveFilePath = DOWNLOAD_SAVE_DIR + "test_post.py"
+        saveFilePath = DOWNLOAD_SAVE_DIR + "test_post2.py"
         #print(saveFilePath)
         with open(saveFilePath,mode="w") as saveFile:
             saveFile.write(post_data)
-#            print(saveFile)
+            print(saveFile)
         subprocess.call(["python", saveFilePath])
+
+
 
 def make_app():
     settings = {
@@ -37,6 +52,8 @@ def make_app():
     return tornado.web.Application([
     (r"/", MainHandler),
     (r'/post_test/', MainHandler_post),
+    (r'/post_test2/', MainHandler_post2),
+    (r'/post_test3/', MainHandler_post3),
 ], **settings)
 
 if __name__ == "__main__":

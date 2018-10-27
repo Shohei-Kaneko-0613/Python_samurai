3d64fe657f07d4fc10ecd1962a77170949d7c3a4
shohei <c373ism@yahoo.co.jp>
2018-10-27 20:32:11 +0900

    ----

--- tornado_HTTP/HTTP_POST.py
+++ tornado_HTTP/HTTP_POST.py
@@ -1,17 +1,20 @@
-import requests
 import urllib.parse
 import yaml
 import os
 import subprocess
+import requests
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+urllib3.disable_warnings(InsecureRequestWarning)
 
 #r1 = requests.post("http://localhost:8888/post_test/", data={"files_data2":open("command.txt","rb").read()})
-r1 = requests.post("http://localhost:8888/post_test/", data={"files_data2":open("command2.txt","rb").read()})
+r1 = requests.post("https://localhost:8443/post_test/",verify=False, data={"files_data2":open("command2.txt","rb").read()})
 response = (r1.content).decode()
 print(response)
 
 if 1 <= len(response):
     print("ファイルあります")
-    r3 = requests.post("http://localhost:8888/post_test2/", data={"files_data3":response})
+    r3 = requests.post("https://localhost:8443/post_test2/",verify=False, data={"files_data3":response})
 elif response == "":
     print("ファイルありません")
     #post_data = "test_post.py"
@@ -32,8 +35,8 @@ elif response == "":
     print(len(data2))
 
 
-    if os.path.exists(data2):
-        r2 = requests.post("http://localhost:8888/post_test3/", files={"files_data":open("static/test_post.py","r")})
+    if os.path.exists(data):
+        r2 = requests.post("https://localhost:8443/post_test3/",verify=False, files={"files_data":open("static/test_post.py","r")})
         print(r2.text,type(r2))
     else:
         print("file is does not exist")
